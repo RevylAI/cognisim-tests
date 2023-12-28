200e07eefba42c40eda8221d3c02c2d88e01a447
Anam Hira <hira.anam49@gmail.com>
2023-12-28 13:30:11 -0800

    updating to run tests

--- dist/index.js
+++ dist/index.js
@@ -2726,7 +2726,7 @@ exports["default"] = _default;
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
 const core = __nccwpck_require__(186)
-const { wait } = __nccwpck_require__(312)
+const httpm = __nccwpck_require__(255)
 
 /**
  * The main function for the action.
@@ -2734,18 +2734,28 @@ const { wait } = __nccwpck_require__(312)
  */
 async function run() {
   try {
-    const ms = core.getInput('milliseconds', { required: true })
+    const testId = core.getInput('test-id', { required: true })
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    // `who-to-greet` input defined in action metadata fil
+    if (!process.env['COGNISIM_API_TOKEN']) {
+      throw Error(
+        'Missing COGNISIM_API_TOKEN get API token from cognisim settings'
+      )
+    }
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const client = new httpm.HttpClient('cognisim-run-action', [], {
+      headers: {
+        Authorization: `Bearer ${process.env['COGNISIM_API_TOKEN']}`,
+        'Content-Type': 'application/json'
+      }
+    })
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    const url = 'https://execution.cognisim.io/execute_test_id'
+    const res = await client.postJson(url, { test_id: testId })
+
+    if (res.message.statusCode !== 200) {
+      throw Error(`Failed to run test: ${res.message.statusMessage}`)
+    }
   } catch (error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error.message)
@@ -2755,30 +2765,6 @@ async function run() {
 module.exports = {
   run
 }
-
-
-/***/ }),
-
-/***/ 312:
-/***/ ((module) => {
-
-/**
- * Wait for a number of milliseconds.
- *
- * @param {number} milliseconds The number of milliseconds to wait.
- * @returns {Promise<string>} Resolves with 'done!' after the wait is over.
- */
-async function wait(milliseconds) {
-  return new Promise(resolve => {
-    if (isNaN(milliseconds)) {
-      throw new Error('milliseconds not a number')
-    }
-
-    setTimeout(() => resolve('done!'), milliseconds)
-  })
-}
-
-module.exports = { wait }
 
 
 /***/ }),
